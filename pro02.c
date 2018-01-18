c1f8df7d393114cacdf1c35fd208b413ffcdb6b8
SI-Abid <33659593+SI-Abid@users.noreply.github.com>
2018-01-18 10:56:30 +0600

    Add files via upload

---
+++ pro02.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i, n, j, m;
+    scanf("%d",&m);
+    for(n=1;n<=m;n++)
+    {
+
+        for(i=m-n;i>0;i--)
+        {
+            printf(" ");
+        }
+        for(i=1;i<=2*n-1;i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(n=m;n>=1;n--)
+    {
+        for(i=n;i<=m;i++)
+        {
+            printf(" ");
+        }
+        for(i=2*n-1;i>=3;i--)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
