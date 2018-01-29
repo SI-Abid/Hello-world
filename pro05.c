28e15748e0c8260561bedd224d6ef8c6d5d18ad9
SI-Abid <33659593+SI-Abid@users.noreply.github.com>
2018-01-29 11:25:23 +0600

    Create pro05.c

---
+++ pro05.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    char a[100];  //initialize 1 d array
+    printf("You:");
+    for (i=0;i<100;i++)
+    {
+        scanf("%c",&a[i]);    //take every single input and put it in a[i]
+        if (a[i]=='\n') break;   //if enter is pressed loop will break
+    }
+    printf("Computer:");
+    for (i=0;i<100;i++)
+    {
+        printf("%c",a[i]);      //show all input taken
+        if (a[i]=='\n') break;  //again loop break
+    }
+    return main();              // the whole code will be looped so user can give as much input as will
+}
