ff617befd0bb082602c5606e162985e8ed6cd28b
Saiham Islam <siaabid5323@gmail.com>
2021-10-14 06:45:41 +0600

    upsolve

---
+++ Codeforces/1593c.cpp
@@ -0,0 +1,64 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define rb rbegin()
+#define re rend()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+class str: public string
+{
+    public:
+    void replace(char x, char y){ for (char &c:*this)if(c==x)c=y; }
+};
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+
+    int t;cin>>t;
+    while(t--)
+    {
+        int n, k;
+        cin>>n>>k;
+        vi v(k);
+        for(int &X:v)
+        {
+            cin>>X;
+            X=n-X;
+        }
+        sort(all(v));   
+        ll m=k, sum=0;
+        for (int i = 0; i < k; i++)
+        {
+            sum+=v[i];
+            if(sum>=n)
+            {
+                m=i;
+                break;
+            }
+        }
+        cout<<m<<endl;
+    }    
+    
+    return 0;
+}
