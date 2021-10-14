dc1e73a46882b1e4d0f12d02c3babdbd6efb4ba8
Saiham Islam <siaabid5323@gmail.com>
2021-10-14 06:22:04 +0600

    obsecured text

---
+++ random/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+    // uint64_t dx = 0x77E435B08;
+    // while (dx) {
+    //     putchar(0x726F6C6564574820 >> (((dx >>= 3) & 7) << 3) & 0xFF);
+    // }
+    int idx[] = {1, 4, 5, 5, 6, 0, 2, 6, 7, 5, 3, -1};
+    for (int *i = idx; *i>-1 ; i++)
+    {
+        fputc(" HWdelor"[*i], stdout);
+    }
+}
--- random/main.c
+++ random/main.c
@@ -1,18 +1,55 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
 
-int main(int argc, char *argv[])
+const char *message = "Hello World";
+#define HIST_CAP 256
+size_t hist[HIST_CAP] = {0};
+#define TABLE_CAP 64
+char table[TABLE_CAP] = {0};
+size_t table_sz = 0;
+#define IDX_CAP 64
+size_t idx[IDX_CAP] = {0};
+size_t idx_sz = 0;
+
+int main()
 {
-    // char *filename = argv[1];
-    // puts(argv[0]);
-    // printf("Welcome Back, ");
-    // puts(argv[1]);
-    // char filename[] = argv[1];
-    // freopen(filename, "r", stdin);
-    int a, b;
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
+    size_t message_sz = strlen(message);
+    for (size_t i = 0; i < message_sz; ++i) {
+        hist[message[i]]++;
+    }
 
-    printf("SUM is %d\n", a+b);
+    for (size_t i = 0; i < HIST_CAP; ++i) {
+        if (hist[i] > 0) {
+            table[table_sz++] = (char) i;
+        }
+    }
+    table[table_sz++] = '\0';
+
+    idx[idx_sz++] = '\0';
+    for(size_t i = 0; i < message_sz; ++i) {
+        for (size_t j = 0; j < table_sz; ++j) {
+            if (table[j] == message[i]) {
+                idx[idx_sz++] = j;
+            }
+        }
+    }
+
+    uint64_t idx64 = 0;
+    for (size_t i = 0; i < idx_sz; ++i) {
+        idx64 = (idx64 << 3) | idx[idx_sz - i - 1];
+    }
+
+    printf("Message: \"%s\"\n", message);
+    printf("Table:   \"%s\"\n", table);
+    printf("Indices: {");
+    for (size_t i = 0; i < idx_sz; ++i) {
+        if (i > 0) printf(", ");
+        printf("%zu", idx[i]);
+    }
+    printf("}\n");
+    printf("Table64:  0x%"PRIX64"\n", *(uint64_t*) table);
+    printf("Idx64:    0x%"PRIX64"\n", idx64);
+
     return 0;
 }
