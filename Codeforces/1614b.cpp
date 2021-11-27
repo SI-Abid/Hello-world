eb6a8e3a8ae12016a155bfc57602897da91d73a8
saiham <siaabid5323@gmail.com>
2021-11-27 11:44:32 +0600

    upsolve

--- Codeforces/1614b.cpp
+++ Codeforces/1614b.cpp
@@ -1,58 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool seen[1000005];
-vector<int> a[1000005];
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
-    if(argc == 3) freopen(argv[2], "w", stdout);
+    if (argc == 2 or argc == 3)
+        freopen(argv[1], "r", stdin);
+    if (argc == 3)
+        freopen(argv[2], "w", stdout);
     ios::sync_with_stdio(false);
-    
+
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int n;
-        cin>>n;
-        for(int i=0;i<1000005;i++)
+        long long n, x;
+        cin >> n;
+
+        vector<pair<long long, long long>> v;
+        for (long long i = 0; i < n; i++)
         {
-            seen[i] = false;
-            a[i].clear();
+            cin >> x;
+            v.push_back(make_pair(x, i + 1));
         }
-        vector<int> v(n), u;
-        for(int i=0;i<n;i++)
+        vector<long long> w(n + 1);
+        sort(v.rbegin(), v.rend());
+        long long ans = 0, cnt = 2, j = 1;
+        w[0] = 0;
+
+        for (auto pp : v)
         {
-            cin>>v[i];
-            a[v[i]].push_back(i+1);
+            long long pos = pp.second;
+            long long vis = pp.first;
+            w[pos] = (cnt / 2) * j;
+            cnt++, j *= -1;
+            ans += abs(w[pos] * vis * 2);
         }
-        vector<int> w(n+1);
-        sort(v.rbegin(),v.rend());
-        int ans=0,cnt=2,j=1;
-        w[0]=0;
         
-        for(int i=0;i<n;i++)
-            if(a[v[i]].size() && !seen[v[i]])
-            {
-                seen[v[i]]=true;
-                for(int pos:a[v[i]])
-                {
-                    w[pos]=(cnt/2)*j;
-                    cnt++;
-                    j*=-1;
-                    ans+=abs(w[pos]*v[i]*2);
-                    // cout<<"ans: "<<ans<<" - v[i] "<<v[i]<<" pos "<<pos<<endl;
-                }
-        }
-       
-        // cout<<cnt<<endl;
-        cout<<ans<<endl;
-        for(int i=0;i<=n;i++)
+        cout << ans << endl;
+        for (long long i = 0; i <= n; i++)
         {
-            cout<<w[i]<<" ";
+            cout << w[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
