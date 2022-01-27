efde624dbaed6bf72d440f4eb491ea5c7207e8da
saiham <siaabid5323@gmail.com>
2022-01-27 23:37:03 +0600

    files added

--- python/hacklead.py
+++ python/hacklead.py
@@ -2,14 +2,18 @@
 # install tkinter: pip3 install tkinter
 # install scrot: sudo apt-get install scrot
 import pyautogui as pag
+import clipboard as cb
 import time
 
 pos1 = (330, 418)
 pos2 = (774, 408)
+pos3 = (274, 502)
 # time.sleep(5)
 # print(pag.position())
 pag.hotkey('alt', 'tab')
 ids = [str(i) for i in range(2012020201, 2012020250)]
+# open file in write mode
+f = open('ids.txt', 'w')
 for id in ids:
     pag.moveTo(pos1)
     pag.click()
@@ -19,3 +23,11 @@ for id in ids:
     pag.moveTo(pos2)
     pag.click()
     time.sleep(15)
+    pag.moveTo(pos3)
+    pag.doubleClick()
+    pag.hotkey('ctrl', 'c')
+    time.sleep(0.5)
+    text=cb.paste()
+    f.write(text)
+    f.write('\n')
+    time.sleep(0.5)
