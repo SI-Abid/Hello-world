35272298f41ae3e75905aa3f3df2ae7d8ccffe53
abid <siaabid5323@gmail.com>
2022-04-28 01:19:46 +0600

    colab

--- Weekly/153a.cpp
+++ Weekly/153a.cpp
@@ -5,8 +5,41 @@ int main()
 {
     string a,b;
     cin>>a>>b;
-    int plus,minux,ques;
-    
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    if(a==b){
+        cout<<"1.0000000000\n";
+        return 0;
+    }
+    int plus,plux,ques;
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        if(b[i]=='?'){
+            ques++;
+        }
+        if(a[i]=='+'){
+            plus++;
+        }
+        else plus--;
+        if(b[i]=='+'){
+            plux++;
+        }
+        else plux--;
+    }
+    double total=1<<ques;
+    double need=0;
+    if(plus>plux) need=plus-plux;
+    else need=plux-plus;
+    //nCr
+    double ans=1;
+    for (size_t i = 1; i <= need; i++)
+    {
+        ans*=(total-i+1);
+        ans/=i;
+    }
+    cout<<fixed<<setprecision(11)<<ans<<"\n";
+    // +++ ++- +-+ -++ +-- -+- --+ --- = 8
+    // ++++ +++- ++-- +--- ---- 3n+2;
     
     return 0;
 }
