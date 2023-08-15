1f0c5929f1043e6abf5479c1d4d316c30b452571
SI-Abid <siaabid5323@gmail.com>
2023-08-15 15:49:36 +0600

    day22-1200

---
+++ Codeforces/1491B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+void solve()
+{
+    int n, u, v;
+    cin >> n >> u >> v;
+    set<int> a;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        a.insert(x);
+    }
+    if (a.size() == 1)1200
+        cout << u + v << nl;
+    else
+        cout << min(u, v) << nl;
+}
+
+signed main()
+{
+    int _(1);
+    cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
