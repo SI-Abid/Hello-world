dd8c7d86a6f9d60ab9f08f764a1ca08945c2d274
SI-Abid <siaabid5323@gmail.com>
2023-08-15 12:55:38 +0600

    day22-1100

--- Codeforces/1574B.py
+++ Codeforces/1574B.py
@@ -1,6 +1,6 @@
 for _ in range(int(input())):
-    a,b,c,m=[int(x) for x in input().split()]
-    if a+b+c-3<m:
+    a, b, c, m = [int(x) for x in input().split()]
+    if a+b+c-3 < m or max(0, 2*max(a, b, c)-a-b-c-1) > m:
         print("NO")
     else:
         print("YES")
