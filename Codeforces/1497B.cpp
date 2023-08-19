6b3f6027dc27d5472f27c577fb335d2b2bf843f6
SI-Abid <siaabid5323@gmail.com>
2023-08-19 12:13:46 +0600

    day26-1100

---
+++ Codeforces/1497B.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < v.size(); i++)
+    {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << ", ";
+    }
+    return os << "]";
+}
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &m)
+{
+    os << "{";
+    for (auto it = m.begin(); it != m.end(); it++)
+    {
+        if (it != m.begin())
+            os << ", ";
+        os << it->first << "-> " << it->second;
+    }
+    return os << "}";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &s)
+{
+    os << "{";
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        if (it != s.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        mp[x % m]++;
+    }
+    vector<int> a;
+    for (auto &&[k, v] : mp)
+    {
+        a.push_back(k);
+    }
+    cout << mp << nl << a << nl;
+    int ans = 0;
+    for (auto &&i : a)
+    {
+        if (mp[i] > 0)
+        {
+            int x = mp[i];
+            int y = mp[m - i];
+            ans++;
+            if (x == y)
+            {
+                mp[i] = mp[m - i] = 0;
+            }
+            else if (x > y)
+            {
+                mp[i] -= (y + 1);
+                mp[m - i] = 0;
+            }
+            else if (x < y)
+            {
+                mp[i] = 0;
+                mp[m - i] -= (x + 1);
+            }
+        }
+    }
+    cout << ans << nl;
+}
+
+signed main()
+{
+    int _(1);
+    cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
--- Codeforces/1857B.cpp
+++ Codeforces/1857B.cpp
@@ -7,6 +7,7 @@ void solve()
 {
     string s;
     cin >> s;
+    int n = s.size();
     // find first character greater than 4
     int i = 0;
     while (i < s.size() and s[i] < '5')
@@ -16,31 +17,38 @@ void solve()
         cout << s << nl;
         return;
     }
-    s = "0"+s;
+    s = "0" + s;
     int cnt = 0;
-    for (int j = i+1; j >= 0; j--)
+    for (int j = n; j > 0; j--)
     {
         if (s[j] >= '5')
         {
-            cnt++;
-            s[j] = '0';
+            cnt = j;
+            // s[j] = '0';
             if (j != 0)
             {
                 s[j - 1] += 1;
             }
         }
     }
-    if(s[0]=='0')s=s.substr(1);
-    i = s.find_first_of('0');
-    if (i == 0)
+    for (int i = cnt; i <= n; i++)
     {
-        s = "1" + string(s.size(), '0');
+        s[i] = '0';
     }
-    else
-    {
-        s = s.substr(0, i + 1) + string(s.size() - i - 1, '0');
-    }
+
+    if (s[0] == '0')
+        s = s.substr(1);
     cout << s << nl;
+    // i = s.find_first_of('0');
+    // if (i == 0)
+    // {
+    //     s = "1" + string(s.size(), '0');
+    // }
+    // else
+    // {
+    //     s = s.substr(0, i + 1) + string(s.size() - i - 1, '0');
+    // }
+    // cout << s << nl;
     // cout<<cnt<<' '<<s<<nl;
 }
 
