becb5c6f97bc70aace48a7fa476bfe14cd15ed22
SI-Abid <siaabid5323@gmail.com>
2023-08-22 12:06:23 +0600

    day29-1400

--- Codeforces/940B.cpp
+++ Codeforces/940B.cpp
@@ -3,22 +3,46 @@ using namespace std;
 #define nl "\n"
 #define ll long long
 
-int exp(int n, int k)
+map<int, int> dp;
+
+int rec(int n, int k, int a, int b)
 {
-    int i=0;
-    for (int cnt = 1; cnt < n; cnt *= k)
-        i++;
-    cout<<i<<" ";
-    return i;
+    if (n == 1)
+        return 0;
+    if (dp.find(n) != dp.end())
+        return dp[n];
+    int ans = 0;
+    if (n % k == 0)
+    {
+        ans = min(rec(n / k, k, a, b) + b, rec(n - 1, k, a, b) + a);
+    }
+    else
+        ans = (rec(n - 1, k, a, b) + a);
+    return dp[n] = ans;
 }
 
 void solve()
 {
     int n, k, a, b;
     cin >> n >> k >> a >> b;
-    int x = (exp(n, k) * b) + ((n % k) * a);
-    int y = (n - 1) * a;
-    cout << min(x, y);
+    // memset(dp, -1, sizeof(dp));
+    // cout << rec(n, k, a, b);
+    int ans=0;
+    while(n>1)
+    {
+        ans+=n%k*a;
+        n-=n%k;
+        if((n-n/k)*a>b)
+        {
+            ans+=b;
+            n/=k;
+        }
+        else{
+            ans+=(n-1)*a;
+            a=1;
+        }
+    }
+    cout<<ans;
 }
 
 signed main()
