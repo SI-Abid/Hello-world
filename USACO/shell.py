490b1ce098532387fed2375145daf61051fc6bd0
SI-Abid <siaabid5323@gmail.com>
2023-08-24 14:58:00 +0600

    simulation

--- USACO/shell.py
+++ USACO/shell.py
@@ -1,8 +1,13 @@
-with open("shell.in","r"):
-	li=[]
-	tr=[]
-	for _ in range(int(input())):
-		a,b,g=map(int,input().split())
-		li.append((a,b))
-		tr.append(g)
-	
+with open("shell.in","r") as f:
+	count=dict()
+	shells=[0,1,2,3]
+	for _ in range(int(f.readline())):
+		a,b,g=map(int,f.readline().split())
+		shells[a],shells[b] = shells[b],shells[a]
+		if shells[g] in count:
+			count[shells[g]]+=1
+		else:
+			count[shells[g]]=1
+	with open('shell.out','w') as ff:
+		ff.write(str(max(count[x] for x in count.keys())))
+
