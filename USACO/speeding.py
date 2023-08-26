11a1827b8eefbc730bbca9095166a521d3039dbc
SI-Abid <siaabid5323@gmail.com>
2023-08-26 12:59:16 +0600

    day33-simulation

---
+++ USACO/speeding.py
@@ -0,0 +1,23 @@
+with open('speeding.in', 'r') as f:
+    n, m = map(int, f.readline().split())
+    road_lim = []
+    for _ in range(n):
+        x, y = map(int, f.readline().split())
+        for __ in range(x):
+            road_lim.append(y)
+    car_speed = []
+    for _ in range(m):
+        x, y = map(int, f.readline().split())
+        for __ in range(x):
+            car_speed.append(y)
+#   input done
+    mx = 0
+    curr_limit = road_lim[0]
+    curr_speed = car_speed[0]
+    for idx in range(100):
+        curr_limit = road_lim[idx]
+        curr_speed = car_speed[idx]
+        if curr_speed > curr_limit:
+            mx = max(mx, curr_speed-curr_limit)
+    with open('speeding.out', 'w') as f:
+        f.write(str(mx))
