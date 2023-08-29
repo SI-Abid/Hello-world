065a527aeb3c39afda8c569465a54644e92db9f8
SI-Abid <siaabid5323@gmail.com>
2023-08-29 17:53:14 +0600

    day36-x

---
+++ Codeforces/1323B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+void solve()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+    vector<int> a(n),b(m);
+    for(auto &i:a)cin>>i;
+    for(auto &i:b)cin>>i;
+    
+}
+
+signed main()
+{
+    int _(1);
+    // cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
