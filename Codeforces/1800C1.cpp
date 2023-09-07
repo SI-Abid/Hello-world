342d3b2c0049b9441323b9766e0291d75254207b
SI-Abid <siaabid5323@gmail.com>
2023-09-07 11:36:28 +0600

    day44-1000

--- Codeforces/1800C1.cpp
+++ Codeforces/1800C1.cpp
@@ -1,53 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define nl "\n"
+#define ll long long
+
+void solve()
+{
+    int n;
+    cin >> n;
+    priority_queue<int> pq;
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 0 and !pq.empty())
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+    cout << ans << nl;
+}
 
 signed main()
 {
-    int _;
-    cin>>_;
-    for(int tc=1;tc<=_;tc++)
+    int _(1);
+    cin >> _;
+    for (int tc = 1; tc <= _; tc++)
     {
         // printf("Case %d: ",tc);
-    
-        int n;
-        cin>>n;
-        vector<long long>arr(n);
-        int heroes=0;
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-            if(arr[i]==0) heroes++;
-        }
-        stack<long long>st;
-        long long ans=0;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==0)
-            {
-                if(!st.empty())
-                {
-                    ans+=st.top();
-                    st.pop();
-                }
-                heroes--;
-            }
-            else
-            {
-                if(st.size()<heroes)
-                {
-                    st.push(arr[i]);
-                }
-                else
-                {
-                    if(st.top()<arr[i])
-                    {
-                        st.pop();
-                        st.push(arr[i]);
-                    }
-                }
-            }
-        }
-        cout<<ans<<endl;
+        solve();
     }
     return 0;
 }
