d2a96822d0d71885818b040c0caf3d50bb395c38
SI-Abid <siaabid5323@gmail.com>
2023-09-07 11:36:44 +0600

    day45-1100

---
+++ Codeforces/1800C2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+void solve()
+{
+    int n;
+    cin >> n;
+    priority_queue<int> pq;
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 0 and !pq.empty())
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+    cout << ans << nl;
+}
+
+signed main()
+{
+    int _(1);
+    cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
