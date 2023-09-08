273e04d1b474ef0f886aec1c7ac974dee65ca86a
SI-Abid <siaabid5323@gmail.com>
2023-09-08 21:25:18 +0600

    1300

---
+++ Codeforces/1100B.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < v.size(); i++)
+    {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << ", ";
+    }
+    return os << "]";
+}
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &m)
+{
+    os << "{";
+    for (auto it = m.begin(); it != m.end(); it++)
+    {
+        if (it != m.begin())
+            os << ", ";
+        os << it->first << "-> " << it->second;
+    }
+    return os << "}";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &s)
+{
+    os << "{";
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        if (it != s.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    n = n * (n + 1) / 2;
+    vector<int> a(m);
+    for (auto &u : a)
+        cin >> u;
+    map<int, int> mp;
+    vector<ll> flag(m + 1, 0);
+    for (auto x : a)
+    {
+        mp[x]++;
+        flag[mp[x]] += x;
+        if (flag[mp[x]] == n)
+            cout << 1;
+        else
+            cout << 0;
+    }
+    // cout<<nl<<mp<<nl<<flag;
+}
+
+signed main()
+{
+    int _(1);
+    // cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
