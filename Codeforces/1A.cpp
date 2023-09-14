455804ab1f1c46a4ce07475056550bfc7a8b38c2
SI-Abid <siaabid5323@gmail.com>
2023-09-14 11:46:25 +0600

    formated

--- Codeforces/1A.cpp
+++ Codeforces/1A.cpp
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n, m, a;
-    scanf("%d %d %d",&n,&m,&a);
+    scanf("%d %d %d", &n, &m, &a);
     long long ans, b, c;
-    b=n/a;
-    if(n%a!=0)
+    b = n / a;
+    if (n % a != 0)
     {
         b++;
     }
-    c=m/a;
-    if(m%a!=0)
+    c = m / a;
+    if (m % a != 0)
     {
         c++;
     }
-    ans=b*c;
+    ans = b * c;
 
-    printf("%lld",ans);
+    printf("%lld", ans);
 
     return 0;
 }
