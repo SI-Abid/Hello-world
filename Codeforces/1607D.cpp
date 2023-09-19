161d0fc4228f4b60e62e29408b092874e084dd47
SI-Abid <siaabid5323@gmail.com>
2023-09-19 17:20:35 +0600

    day57-x

---
+++ Codeforces/1607D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    string s;
+    for (auto &x : a)
+        cin >> x;
+    cin >> s;
+    
+}
+
+signed main()
+{
+    int _(1);
+    cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
