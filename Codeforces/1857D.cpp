c876ba6b66a99f2f9cdee667db9ec69566c26c71
SI-Abid <siaabid5323@gmail.com>
2023-10-02 11:09:35 +0600

    day70-1300

---
+++ Codeforces/1857D.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < v.size(); i++)
+    {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << ", ";
+    }
+    return os << "]";
+}
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &m)
+{
+    os << "{";
+    for (auto it = m.begin(); it != m.end(); it++)
+    {
+        if (it != m.begin())
+            os << ", ";
+        os << it->first << "-> " << it->second;
+    }
+    return os << "}";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &s)
+{
+    os << "{";
+    for (auto it = s.begin(); it != s.end(); it++)
+    {
+        if (it != s.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a)
+        cin >> x;
+    for (size_t i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        a[i] -= x;
+    }
+    int maxi = *max_element(a.begin(), a.end());
+    vector<int> ans;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (a[i] == maxi)
+            ans.push_back(i + 1);
+    }
+    cout << ans.size() << nl;
+    for (auto x : ans)
+        cout << x << ' ';
+    cout << nl;
+}
+
+signed main()
+{
+    int _(1);
+    cin >> _;
+    for (int tc = 1; tc <= _; tc++)
+    {
+        // printf("Case %d: ",tc);
+        solve();
+    }
+    return 0;
+}
