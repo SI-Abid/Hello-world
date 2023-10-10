84552ad32ab5419e7ec6ba28b8d31b5477fa2927
SI-Abid <siaabid5323@gmail.com>
2023-10-10 09:16:55 +0600

    day78-x

---
+++ Codeforces/1299A.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] a = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a, Collections.reverseOrder());
+        for (int i : a) {
+            System.out.print(i+" ");
+        }
+    }
+}
