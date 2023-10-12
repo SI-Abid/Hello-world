0579697bb2415259aa587177bf5e2c38f42cacd3
SI-Abid <siaabid5323@gmail.com>
2023-10-12 21:22:37 +0600

    1300

--- Codeforces/1826C.cpp
+++ Codeforces/1826C.cpp
@@ -1,19 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
+int div(int n)
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return i;
+    }
+    return n;
+}
 signed main()
 {
     int _;
-    cin>>_;
-    for(int tc=1;tc<=_;tc++)
+    cin >> _;
+
+    for (int tc = 1; tc <= _; tc++)
     {
         // printf("Case %d: ",tc);
-    
-        int n,m;
-        cin>>n>>m;
-        if(__gcd(n,m)==1) cout<<"YES\n";
-        else cout<<"NO\n";
-    
+
+        int n, m;
+        cin >> n >> m;
+        if (n>1 and div(n) <= m)
+            puts("NO");
+        else
+            puts("YES");
     }
     return 0;
 }
