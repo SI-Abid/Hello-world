4524b331a2ebdf4cf5f1b2b4f2c5da3f7d569af2
SI-Abid <siaabid5323@gmail.com>
2023-10-16 12:40:00 +0600

    day84-1500

--- Codeforces/1858B.cpp
+++ Codeforces/1858B.cpp
@@ -1,14 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 #define nl "\n"
 #define ll long long
 
 void solve()
 {
-    int n,m,d;
-    cin>>n>>m>>d;
+    int n, m, d;
+    cin >> n >> m >> d;
     vector<int> a(m);
-    
+    for (auto &x : a)
+        cin >> x, x--;
+    auto cookies_between = [&](int x, int y) -> int
+    {
+        if (x >= y)
+            return 0;
+
+        return (y - x - 1) / d;
+    };
+    int tot = m;
+    for (int i = 0; i < m; i++)
+    {
+        int bfr = i == 0 ? -d : a[i - 1];
+        tot += cookies_between(bfr, a[i]);
+    }
+
+    tot += cookies_between(a.back(), n);
+    int max = -1e9, choice = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int bfr = i == 0 ? -d : a[i - 1];
+        int atr = i == m - 1 ? n : a[i + 1];
+        int lost = cookies_between(bfr, a[i]) + 1 + cookies_between(a[i], atr) - cookies_between(bfr, atr);
+        if (lost > max)
+        {
+            max = lost;
+            choice = 1;
+        }
+        else if (lost == max)
+        {
+            choice++;
+        }
+    }
+    cout << tot - max << " " << choice << nl;
 }
 
 signed main()
