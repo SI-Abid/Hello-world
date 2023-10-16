fba96817a88bb0c51bf086a70f4173e7df152baa
SI-Abid <siaabid5323@gmail.com>
2023-10-16 12:26:44 +0600

    day84-1400

--- Codeforces/1573B.cpp
+++ Codeforces/1573B.cpp
@@ -7,7 +7,7 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(n + 1), b(n + 1);
+    vector<int> a(2*n + 1), b(2*n + 1);
     for (int i = 0; i < n; i++)
     {
         cin >> a[i + 1];
