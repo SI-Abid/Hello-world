9fa6da5b29116c60c26f4bbf21d75504f1bb19ff
SI-Abid <siaabid5323@gmail.com>
2023-10-22 11:22:21 +0600

    day90-1300

--- Codeforces/1692F.cpp
+++ Codeforces/1692F.cpp
@@ -7,45 +7,27 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(n);
-    map<int, int> mod;
-    for (auto &x : a)
+    vector<int> a;
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
     {
+        int x;
         cin >> x;
-        mod[x % 10]++;
+        mp[x % 10]++;
     }
     for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            for (int k = 0; k < 10; k++)
-            {
-                if ((i + j + k) % 10 == 3)
+        for (int j = 0; j < min(mp[i], 3); j++)
+            a.push_back(i);
+
+    for (int i = 0; i < a.size(); i++)
+        for (int j = i + 1; j < a.size(); j++)
+            for (int k = j + 1; k < a.size(); k++)
+                if ((a[i] + a[j] + a[k]) % 10 == 3)
                 {
-                    if (i == j and mod[i] > 1 and mod[k] > 0)
-                    {
-                        puts("YES");
-                        return;
-                    }
-                    if (i == k and mod[i] > 1 and mod[j] > 0)
-                    {
-                        puts("YES");
-                        return;
-                    }
-                    if (k == j and mod[k] > 1 and mod[i] > 0)
-                    {
-                        puts("YES");
-                        return;
-                    }
-                    if (i != j and j != k and i!=k and mod[i] and mod[j] and mod[k])
-                    {
-                        puts("YES");
-                        return;
-                    }
+                    puts("YES");
+                    return;
                 }
-            }
-        }
-    }
+
     puts("NO");
 }
 
